84c3103fa107bc19d4d528d4388ea8b06caa991d
KyuJinLee0911 <rbwls1001@naver.com>
2024-05-09 20:29:13 +0900

    2024.05.09
    - nav mesh를 이용하여 클릭을 통한 캐릭터 이동 구현
    - 마우스 휠을 이용한 카메라 줌인/줌아웃 구현
    - 마우스 휠 클릭을 이용해 카메라 돌리기 구현
    - 캐릭터 따라다니는 카메라 구현
    - Blend tree를 사용해 속도에 따라 달라지는 애니메이션 적용

---
+++ RPG Project/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField]
+    Transform target;
+
+    [SerializeField]
+    Transform camTransform;
+    public float distance;
+    Vector3 distVec;
+
+    [SerializeField]
+    float minDist = 5f;
+    [SerializeField]
+    float maxDist = 25f;
+
+    RPGProject playerInput;
+    InputAction look;
+    InputAction zoom;
+
+    public float camRotSpeed = 0.15f;
+
+    private void Awake()
+    {
+        playerInput = new RPGProject();
+        playerInput.Player.Enable();
+        look = playerInput.Player.Look;
+        zoom = playerInput.Player.Zoom;
+    }
+
+    private void OnEnable()
+    {
+
+        look.started += OnLook;
+        look.performed += OnLook;
+        look.canceled += OnLook;
+
+        zoom.performed += OnZoom;
+    }
+
+    private void Start()
+    {
+        camTransform = transform.GetChild(0);
+        distVec = camTransform.position - transform.position;
+        distance = Vector3.Distance(camTransform.position, target.position);
+
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = target.position;
+        camTransform.localPosition = distVec.normalized * distance;
+    }
+
+    private void OnDisable()
+    {
+
+        look.started -= OnLook;
+        look.performed -= OnLook;
+        look.canceled -= OnLook;
+
+        zoom.performed -= OnZoom;
+
+        playerInput.Player.Disable();
+    }
+
+    void OnZoom(InputAction.CallbackContext context)
+    {
+        distance -= context.ReadValue<float>() * 0.01f;
+        if (distance > maxDist)
+            distance = maxDist;
+
+        if (distance < minDist)
+            distance = minDist;
+    }
+
+
+    float beforeValue;
+    float afterValue;
+    void OnLook(InputAction.CallbackContext context)
+    {
+        beforeValue = context.ReadValue<float>();
+
+        if(context.performed)
+        {
+            if(Mathf.Abs(beforeValue - afterValue) > 0.001f)
+                transform.localRotation = Quaternion.Euler(0, transform.localRotation.eulerAngles.y + (beforeValue - afterValue) * camRotSpeed, 0);
+        }
+        
+        afterValue = context.ReadValue<float>();
+    }
+}
